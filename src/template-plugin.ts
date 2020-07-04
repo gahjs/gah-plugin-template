25ba6503ae057591bc443021c9f370c33fcbbdae
Janik Schumacher <rndmb0t@gmail.com>
2020-07-04 16:32:41 +0200

    example plugin

---
+++ src/template-plugin.ts
@@ -0,0 +1,81 @@
+import { GahPlugin, GahEvent, GahPluginConfig, InstallStartedEvent, FinishedgModuleInstallEvent } from '@awdware/gah-shared';
+
+import { TemplateConfig } from './template-config';
+
+/**
+ * A gah plugin has to extend the abstract GahPlugin base class and implement the abstract methods.
+ */
+export class TemplatePlugin extends GahPlugin {
+  constructor() {
+    // Call the constructor with the name of the plugin (only used for logging, does not need to match the package name)
+    super('TemplatePlugin');
+  }
+
+  /**
+   * Called after adding the plugin with gah. Used to configure the plugin.
+   * @param existingCfg This will be passed by gah and is used to check wheter a property is already configured or not
+   */
+  protected async onInstall(existingCfg: TemplateConfig): Promise<GahPluginConfig> {
+    // Create a new instance of the plugin configuration
+    const newCfg = new TemplateConfig();
+
+    // Ask the user for configuration after installing the plugin. ONLY if the values do not exist yet!
+    newCfg.someSetting = await this.promptService.input({
+      msg: 'Please enter a string configuration property',
+      default: 'default value',
+      enabled: () => !(existingCfg?.someSetting),
+      cancelled: false,
+      validator: (val: string) => val.endsWith('.json')
+    }) ?? existingCfg.someSetting; // Defaults back to the old value in case undefined gets returned
+
+    // Ask the user for configuration after installing the plugin. ONLY if the values do not exist yet!
+    newCfg.somePathSetting = await this.promptService.fuzzyPath({
+      msg: 'Please enter a (fuzzy)path configuration property',
+      default: 'test/directory',
+      enabled: () => !(existingCfg?.somePathSetting),
+      cancelled: false,
+      itemType: 'file'
+    }) ?? existingCfg.somePathSetting; // Defaults back to the old value in case undefined gets returned
+
+    // Ask the user for configuration after installing the plugin. ONLY if the values do not exist yet!
+    newCfg.someArraySetting = await this.promptService.checkbox({
+      msg: 'Please enter a (fuzzy)path configuration property',
+      default: 'test/directory',
+      enabled: () => !(existingCfg?.someArraySetting),
+      cancelled: false,
+      choices: () => ['Option1', 'Option2', 'Option3', 'Option4', 'Option5']
+    }) ?? existingCfg.someArraySetting; // Defaults back to the old value in case undefined gets returned
+
+    // Return the new (or maybe unchanged) config back to gah for saving!
+    return newCfg;
+  }
+
+  /**
+   * Called everytime gah gets used for all configured plugins. Register your handlers here.
+   */
+  public onInit() {
+    // Register a handler that gets called synchronously if the corresponding event occured. Some events can be called multiple times!
+    this.registerEventListener(GahEvent.INSTALL_STARTED, (event: InstallStartedEvent) => {
+      // Some example logic that does not really do anything:
+      if (!event.gahFile?.isHost) {
+        return;
+      }
+      console.log(this.cfg.someSetting + ' --> ' + event.gahFile?.modules[0].moduleName!);
+    });
+
+    this.registerEventListener(GahEvent.FINISHED_MODULE_INSTALL, (event: FinishedgModuleInstallEvent) => {
+      // Some example logic that does not really do anything:
+      if (!event.module?.isEntry) {
+        return;
+      }
+      console.log('entry module: ' + event.module.moduleName!);
+    });
+  }
+
+  /**
+   * For convenience the correctly casted configuration
+   */
+  private get cfg() {
+    return this.config as TemplateConfig;
+  }
+}
